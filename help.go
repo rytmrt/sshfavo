8be70bb52e33a519aa2d32ac72b6476a6a499632
Ryota Morita <ryota.morita.3.8@gmail.com>
2015-10-06 16:58:38 +0900

    ヘルプを更新

--- help.go
+++ help.go
@@ -22,16 +22,26 @@ func (self *Help) Run() (err error) {
 	list := NewList(buf.String())
 	serverList, _ := list.GetServerList()
 
-	fmt.Printf("usage: sshfavo [-h] [-e server_name] [-l server_name]\n")
+	fmt.Printf("Usage: sshfavo [options]\n")
 	fmt.Printf("\n")
-	fmt.Printf("+-------------------------------------------------------------------------+\n")
-	fmt.Printf("| Server list                                                             |\n")
-	fmt.Printf("+-------------------------------------------------------------------------+\n")
+	fmt.Printf("OPTIONS:\n")
+	fmt.Printf("-v, --version\n")
+	fmt.Printf("\n")
+	fmt.Printf("-h, --help\n")
+	fmt.Printf("\n")
+	fmt.Printf("-l, --login SERVER_NAME\n")
+	fmt.Printf("  ssh login.\n")
+	fmt.Printf("\n")
+	fmt.Printf("-e, --edit SERVER_NAME\n")
+	fmt.Printf("  Edit server config using vi.\n")
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Printf("Favorite server list:\n")
 	if len(serverList) <= 0 {
-		fmt.Printf("You have not Server.toml!\n")
+		fmt.Printf("  Favorite sever is empty!\n")
 	} else {
 		for i := range serverList {
-			fmt.Printf(" + %s\n", serverList[i])
+			fmt.Printf("  - %s\n", serverList[i])
 		}
 	}
 	return
